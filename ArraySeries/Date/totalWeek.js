4143f638e71b0c1b1e7138d858ab4c0c92fd0b57
Shubham Domir <shubhamdomir4@gmail.com>
2023-08-21 23:29:12 +0530

    leetcode

---
+++ ArraySeries/Date/totalWeek.js
@@ -0,0 +1,23 @@
+/**
+ * Returns number of weeks
+ *
+ * @param {Number} year - full year (2018)
+ * @param {Number} month - zero-based month index (0-11)
+ * @param {Boolean} fromMonday - false if weeks start from Sunday, true - from Monday.
+ * @returns {number}
+ */
+const weeksInMonth = (year, month, fromMonday = false) => {
+    const first = new Date(year, month, 1); // get first date of the month
+    const last  = new Date(year, month + 1, 0); // get last date of the month
+    console.log('first',first);
+     console.log('last',last);
+    let dayOfWeek = first.getDay(); // get day on 1st of the month
+    console.log('dayOfWeek',dayOfWeek);
+    console.log('last.getDate();',last.getDate()); // get date 30 or 31 of the month
+    if (fromMonday && dayOfWeek === 0) dayOfWeek = 7; // make sunday as last day of week
+    let days = dayOfWeek + last.getDate(); // add days left in forming a week and total days in a month
+    console.log('days',days);
+    if (fromMonday) days -= 1; // if week start from monday then subtract 1 from days
+    return Math.ceil(days / 7);
+}
+console.log(weeksInMonth(2023,6, false)); // for july
---
+++ ArraySeries/secondLargestElement.js
@@ -0,0 +1,53 @@
+// Javascript program to find second largest
+// element in an array
+// 1) Initialize the first as 0(i.e, index of arr[0] element
+//     2) Start traversing the array from array[1],
+//        a) If the current element in array say arr[i] is greater
+//           than first. Then update first and second as,
+//           second = first
+//           first = arr[i]
+//        b) If the current element is in between first and second, and is greater than second element
+//           then update second to store the value of current variable as
+//           second = arr[i]
+//     3) Return the value stored in second.
+    // Function to print the second largest elements
+    function print2largest(arr, arr_size) {
+        let i;
+        let largest = second = -Infinity;
+   
+        // There should be atleast two elements
+        if (arr_size < 2) {
+            document.write(" Invalid Input ");
+            return;
+        }
+   
+        // finding the largest element
+         
+        for (i = 0 ;i<arr_size;i++){
+            if (arr[i]>largest){
+                second = largest ;
+                largest = arr[i]
+            }
+ 
+            else if (arr[i]!=largest && arr[i]>second ){
+                second = arr[i];
+            }
+        }
+  
+        if (second == -2454635434){
+             
+        console.log("There is no second largest element<br>");
+        }
+        else{
+            console.log("The second largest element is " + second);
+                return;
+            }
+        }
+     
+ 
+    // Driver program to test above function
+  
+    let arr= [ 12, 35, 1, 10, 34, 1 ];
+    let n = arr.length;
+    print2largest(arr, n);
+     
